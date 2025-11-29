4e93cacf4e5f87826021e00e9c5244a01bd89f37
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2025-11-29 15:49:25 +0600

    Updated

--- client/customer-frontend-nextjs/components/ui/buttons/search-button.component.tsx
+++ client/customer-frontend-nextjs/components/ui/buttons/search-button.component.tsx
@@ -3,7 +3,7 @@ import { IoSearch } from "react-icons/io5";
 
 export default function SearchButton() {
   return (
-    <div className="bg-[color:var(--primary-pink)] text-[color:var(--primary-white)] font-bold rounded-full p-3 cursor-pointer hover:shadow-md transition-shadow duration-200">
+    <div className="bg-[color:var(--primary-pink)] text-[color:var(--primary-white)] font-bold rounded-full p-4 cursor-pointer hover:shadow-md transition-shadow duration-200">
       <IoSearch size={20} />
     </div>
   );
--- client/customer-frontend-nextjs/components/ui/search-bar/search-bar.component.tsx
+++ client/customer-frontend-nextjs/components/ui/search-bar/search-bar.component.tsx
@@ -3,17 +3,41 @@
 import SearchButton from "../buttons/search-button.component";
 
 export default function SearchBar() {
+  const serachBarContent = [
+    {
+      label: "Where",
+      subLabels: "Search destinations",
+    },
+    { label: "When", subLabels: "Add dates" },
+    { label: "Who", subLabels: "Add guests" },
+  ];
   return (
-    <div className="min-w-[850px] max-h-[66px] shadow-lg border border-gray-300 bg-[#fff] rounded-full px-6 py-2 hover:shadow-md transition-shadow duration-200">
+    <div className="min-w-[850px] min-h-[66px] shadow-lg border border-gray-300 bg-[#fff] rounded-full px-2 py-2 hover:shadow-md transition-shadow duration-200">
       <div className="flex flex-row gap-2 justify-between items-center">
-        <div className="">Where</div>
-        <div className="w-[1px] h-6 bg-gray-300"></div>
-        <div className="">Check In</div>
-        <div className="w-[1px] h-6 bg-gray-300"></div>
-        <div className="">Check Out</div>
-        <div className="w-[1px] h-6 bg-gray-300"></div>
-        <div className="">Add guests</div>
-        <SearchButton />
+        {serachBarContent.map((item, index) => (
+          <div
+            key={index}
+            className={`flex-1 flex flex-row justify-between items-center ${
+              index < serachBarContent.length - 1
+                ? "border-r border-gray-300"
+                : ""
+            }`}
+          >
+            <div
+              className={`flex flex-col justify-center items-start gap-1 ${
+                index === 0 ? "ml-4" : ""
+              }`}
+            >
+              <span className="text-xs font-medium text-[var(--text-primary)]">
+                {item.label}
+              </span>
+              <span className="text-xs text-[var(--text-secondary)]">
+                {item.subLabels}
+              </span>
+            </div>
+            {index === serachBarContent.length - 1 && <SearchButton />}
+          </div>
+        ))}
       </div>
     </div>
   );
