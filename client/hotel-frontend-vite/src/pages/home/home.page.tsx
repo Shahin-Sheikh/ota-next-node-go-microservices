c98d095b518b3c71a42ce7727f7d73fcd811d740
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2025-12-02 12:57:30 +0600

    added hotel frontend

---
+++ client/hotel-frontend-vite/src/pages/home/home.page.tsx
@@ -0,0 +1,19 @@
+export default function HomePage() {
+  return (
+    <div className="grid grid-cols-2 ">
+      <div className="col-span-1">
+        <img
+          src="https://cdn.pixabay.com/photo/2025/11/08/13/23/zebra-9944593_1280.jpg"
+          alt="Zebra"
+        />
+      </div>
+      <br />
+      <div className="col-4">
+        <img
+          src=" https://cdn.pixabay.com/photo/2025/08/15/06/29/mountain-cabin-9776289_640.jpg"
+          alt="Zebra"
+        />
+      </div>
+    </div>
+  );
+}
